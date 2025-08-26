a0443890cf5f4edb731c8213dda76138fe45c4b2
Siddharth-commet <sid.siddharth2907@gmail.com>
2025-08-26 15:50:14 +0530

    abc

--- week2/Q8.cpp
+++ week2/Q8.cpp
@@ -9,7 +9,6 @@ int countDistinct(vector<int>& arr) {
 int main() {
     vector<int> arr = {10, 20, 10, 30, 20, 40, 50, 30};
     int n = arr.size();
-
     cout << "Total distinct elements = " << countDistinct(arr) << endl;
     return 0;
 }
